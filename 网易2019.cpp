5bcca2edb692d65956f0ce21d920b468ffbb5ae5
yuzhizhi <493801414@qq.com>
2018-04-13 16:58:48 +0800

    新增一道题目，但是矩形重叠没有做

--- 网易2019.cpp
+++ 网易2019.cpp
@@ -393,3 +393,58 @@ int main() {
 		cout << cnt << endl;
 	}
 }
+
+/*
+迷路的牛牛
+
+牛牛去犇犇老师家补课，出门的时候面向北方，但是现在他迷路了。虽然他手里有一张地图，但是他需要知道自己面向哪个方向，请你帮帮他。
+输入描述:
+每个输入包含一个测试用例。
+每个测试用例的第一行包含一个正整数，表示转方向的次数N(N<=1000)。
+接下来的一行包含一个长度为N的字符串，由L和R组成，L表示向左转，R表示向右转。
+
+输出描述:
+输出牛牛最后面向的方向，N表示北，S表示南，E表示东，W表示西。
+示例1
+输入
+3
+LRR
+输出
+E
+*/
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	string s;
+	cin >> s;
+	int i = 0,dir=0;
+	while (i < N) {
+		switch (s[i]) {
+		case 'L': {
+			dir += 1;
+			break;
+		}
+		case 'R': {
+			dir -= 1;
+			break;
+		}
+		}
+		if (dir >= 4)dir %= 4;
+		if (dir < 0)dir += 4;
+		++i;
+	}
+	switch (dir) {
+	case 0:cout << 'N' << endl; break;
+	case 1:cout << 'W' << endl; break;
+	case 2:cout << 'S' << endl; break;
+	case 3:cout << 'E' << endl; break;
+	}
+}
+/*
+矩形重叠：没有思路。。。。
+*/
